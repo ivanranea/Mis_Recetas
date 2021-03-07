17e250e486746d6a52ba386660cca649ee2299da
ivan <ivanranea@gmail.com>
2021-03-07 14:33:26 -0300

    sprint 1-1

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -16,8 +16,40 @@ var app = new Framework7({
     // Add default routes
     routes: [
       {
-        path: '/about/',
-        url: 'about.html',
+        path: '/registro/',
+        url: 'registro.html',
+      },
+	  {
+        path: '/principal/',
+        url: 'principal.html',
+      },
+	  {
+        path: '/index/',
+        url: 'index.html',
+      },
+	  {
+        path: '/categElegida/',
+        url: 'categElegida.html',
+      },
+	  {
+        path: '/crearCateg/',
+        url: 'crearCateg.html',
+      },
+	  {
+        path: '/crearReceta/',
+        url: 'crearReceta.html',
+      },
+	  {
+        path: '/favoritos/',
+        url: 'favoritos.html',
+      },
+	  {
+        path: '/perfil/',
+        url: 'perfil.html',
+      },
+	  {
+        path: '/buscador/',
+        url: 'buscador.html',
       },
     ]
     // ... other parameters
@@ -28,17 +60,51 @@ var mainView = app.views.create('.view-main');
 // Handle Cordova Device Ready Event
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
+	
+	
 });
 
 // Option 1. Using one 'page:init' handler for all pages
 $$(document).on('page:init', function (e) {
     // Do something here when page loaded and initialized
-    console.log(e);
+
 })
 
 // Option 2. Using live 'page:init' event handlers for each page
-$$(document).on('page:init', '.page[data-name="about"]', function (e) {
+$$(document).on('page:init', '.page[data-name="registro"]', function (e) {
     // Do something here when page with data-name="about" attribute loaded and initialized
-    console.log(e);
-    alert('Hello');
+
+	
+	$$("#btnregistro").on("click", fnRegistrarUsuario);
+
 })
+
+function fnRegistrarUsuario(){
+	
+	nombre = $$("#nombregistro").val();
+	email = $$("#emailregistro").val();
+	password = $$("#passregistro").val();
+	
+	console.log("nombre: "+ nombre +", email: "+email+", pass: "+ password);
+	
+/*	firebase.auth().createUserWithEmailAndPassword(email, password)
+	.then(function (){
+		
+		//mandar mail de verificacion
+		
+	})
+	.catch(function(error) {
+	// Handle Errors here.
+	var errorCode = error.code;
+	var errorMessage = error.message;
+	if (errorCode == 'auth/weak-password') {
+	alert('Clave muy débil.');
+	} else {
+	alert(errorMessage);
+	}
+	console.log(error);
+	});
+	alert("q paso?")*/
+}
+
+
